f53d8075ed101d9f2071ddde7356ffe358152cf7
Daniel Jaramillo <dan.jaramillo@gmail.com>
2023-03-17 15:11:51 -0700

    Completed tare scale and measure of LoadSensor.

--- src/LoadSensor.cpp
+++ src/LoadSensor.cpp
@@ -7,19 +7,44 @@ LoadSensor::LoadSensor(uint32_t d_pin, uint32_t c_pin, int32_t offset) : HX711(d
 LoadSensor::LoadSensor(uint32_t d_pin, uint32_t c_pin, int32_t offset, float scale) : HX711(d_pin, c_pin), m_offset(offset), m_scale(scale) {}
 
 int32_t LoadSensor::tare(uint16_t samples = 16) {
-    int32_t offset = 0, temp = 0;
+    int32_t temp = 0;
     for(int i = 0; i < samples; i++) {
         temp = read();
         if(temp == INT32_MIN) {
+            // Check for error with HX711 Sensor
             return temp;
         }
-        offset += temp;
+        m_offset += temp;
     }
-    offset /= samples;
-    m_offset = offset;
-    return offset;
+    m_offset /= samples;
+    return m_offset;
 }
 
-float LoadSensor::scale(float test_load, uint16_t samples = 16) {}
+float LoadSensor::scale(float test_load, uint16_t samples = 16) {
+    int32_t measurement = 0, temp = 0;
+    for(int i = 0; i < samples; i++) {
+        temp = read();
+        if(temp == INT32_MIN) {
+            // Check for error with HX711 Sensor
+            return temp;
+        }
+        measurement += temp;
+    }
+    m_scale = test_load / (float)(measurement / samples - m_offset);
+    return m_scale;
+}
 
-float LoadSensor::measure(uint16_t samples = 16);
+float LoadSensor::measure(uint16_t samples = 16) {
+    int32_t measurement = 0, temp = 0;
+    for(int i = 0; i < samples; i++) {
+        temp = read();
+        if(temp == INT32_MIN) {
+            // Check for error with HX711 Sensor
+            return temp;
+        }
+        measurement += temp;
+    }
+    measurement /= samples;
+
+    return (measurement - m_offset) * m_scale;
+}
--- src/LoadSensor.h
+++ src/LoadSensor.h
@@ -9,6 +9,8 @@ class LoadSensor: public HX711 {
     public:
         /**
          * Constructors
+         * All constructors require setting the data pin and the clock pin to use the underlaying HX711 ADC. 
+         * Added constructors allow setting the offset and scale factors.
         */
         LoadSensor(uint32_t d_pin, uint32_t c_pin);
         LoadSensor(uint32_t d_pin, uint32_t c_pin, int32_t offset);
@@ -23,13 +25,14 @@ class LoadSensor: public HX711 {
 
         /**
          * scale()
-         * Sets the scale factor to calibrate the load sensor.
+         * Sets the scale factor to calibrate the load sensor. Offset should be calibrated first.
         */
         float scale(float test_load, uint16_t samples = 16);
 
         /**
          * measure()
-         * 
+         * Using the scale factor and offset, calculates the force applied to the load cell. 
+         * Offset and scale factor should be set first.
         */
         float measure(uint16_t samples = 16);
 
